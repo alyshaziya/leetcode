9c3cb95d10ed224df1f7cc67bed3107797091b99
alyshaziya <alyshaziya@icloud.com>
2022-09-05 10:38:14 +1000

    day 5, two pointer

---
+++ algorithms-I/day-5-twopointer4.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        nodes = []
+        
+        while head != None:
+            nodes.append(head)
+            head = head.next
+        return nodes[len(nodes)//2]
+        
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast, slow = head, head
+        for _ in range(n): fast = fast.next
+        if not fast: return head.next
+        while fast.next: fast, slow = fast.next, slow.next
+        slow.next = slow.next.next
+        return head
+
+        
